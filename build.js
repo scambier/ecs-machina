bedbde1abba1208c74298fbc947dc1f7481db8b1
Simon Cambier <simoncambier@fastmail.com>
2020-08-18 10:50:25 +0200

    Cleaner publish process

--- build.js
+++ build.js
@@ -1,7 +1,6 @@
 const distFolder = 'dist'
 
 const shell = require('shelljs')
-const fs = require('fs')
 
 // Delete distFolder
 console.log('... Removing ' + distFolder + ' folder')
@@ -10,17 +9,5 @@ shell.rm('-rf', distFolder)
 // Build typescript
 console.log('... Building TypeScript')
 shell.exec('tsc')
-
-// Write package.json as a public module
-console.log('... Copying files')
-const pkg = require('./package.json')
-delete pkg.private // Make dist folder public
-delete pkg.scripts
-fs.writeFileSync(distFolder + '/package.json', JSON.stringify(pkg, null, 2), 'utf8')
-
-// Copy other files
-// shell.cp('-rf', 'package-lock.json', distFolder)
-shell.cp('-rf', 'README.md', distFolder)
-// shell.cp('-rf', 'src/@types', path.join(distFolder, '@types'))
 
 console.log('Done.')
